8cd3635cab2aab845a296b26e8b08746c310eddd
Zainabraja <zainabraja53@gmail.com>
2021-10-06 16:38:28 +0530

    commit

--- register.php
+++ register.php
@@ -32,7 +32,7 @@
             $str="insert into user set name='$name',email='$email',password='$password',college='$college'";
 			if((mysqli_query($con,$str)))	
 			echo "<center><h3><script>alert('Congrats.. You have successfully registered !!');</script></h3></center>";
-			header('location: welcome.php?q=1');
+			header('location:welcome.php?q=1');
 		}
     }
 ?>
