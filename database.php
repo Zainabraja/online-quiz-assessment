8bdee95691c66def009b760dcbd50ccd607fd1f7
Zainabraja <zainabraja53@gmail.com>
2021-10-06 17:22:00 +0530

    commit

--- database.php
+++ database.php
@@ -1,4 +1,4 @@
 <?php
-$con= new mysqli('https://online-quiz-assessment.herokuapp.com/','root','','exam')or die("Could not connect to mysql".mysqli_error($con));
+$con= new mysqli('online-quiz-assessment.herokuapp.com/','root','','exam')or die("Could not connect to mysql".mysqli_error($con));
 
 ?>
